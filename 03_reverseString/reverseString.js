2e130800ec1e24997092f92c489a90d0c40e4812
Mugdha <u19cse1001@cit.ac.in>
2022-04-30 23:18:41 +0530

    String reverse tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const reversedStr = [];
+    for(let i = 0; i< str.length;i++) {
+        reversedStr[i] = str.charAt(str.length - i - 1);
+    }
+    return reversedStr.join("");
 };
 
 // Do not edit below this line
