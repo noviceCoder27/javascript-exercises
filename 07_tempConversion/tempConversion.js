e8438739fe0e4efa81eda4137315e64b00705107
Mugdha <u19cse1001@cit.ac.in>
2022-05-23 22:57:03 +0530

    TempConversion Tests Passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  return Math.round(((5/9) * (temp - 32)) * 10)/10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Math.round((temp * (9/5) + 32) *10)/10;
 };
 
 // Do not edit below this line
