3a6d90fa8070db32fe3c0fcde97a9ea910624d6a
Mugdha <u19cse1001@cit.ac.in>
2022-05-23 22:17:48 +0530

    RemoveFromArray tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+    for(let i = 0; i< args.length; i++) {
+        for(let j = 0; j< arr.length; j++) {
+            if(arr[j] === args[i]) {
+                arr.splice(j,1);
+            }
+        }
+    }
+    return arr;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
